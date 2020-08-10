cea4e421a0bdf37df638e37f3fc01ee0e617cb96
Zvonimir Pervan <z.pervan65@gmail.com>
2020-08-10 23:11:37 +0200

    Legend: Add skeleton class with todo

---
+++ Plot/Common/src/legend.h
@@ -0,0 +1,11 @@
+#ifndef PLOTINGZ_LEGEND_H
+#define PLOTINGZ_LEGEND_H
+
+
+class Legend {
+public:
+    /// @todo: For every XY data, there should be a entry in the legend, also, the appropriate colour is used for each data point
+};
+
+
+#endif //PLOTINGZ_LEGEND_H
