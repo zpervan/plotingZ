b3454ded3d6eca213562c2ada8fa7c6105a8686f
Zvonimir Pervan <z.pervan65@gmail.com>
2020-08-18 09:41:15 +0200

    Legend: Add basic labeling logic

--- Plot/Common/src/legend.cpp
+++ Plot/Common/src/legend.cpp
@@ -1,1 +1,34 @@
-#include "legend.h"
+#include "Plot/Common/src/legend.h"
+
+void Legend::SetLegendLabels(const std::vector<std::string> &labels) {
+    labels_.reserve(labels.size());
+    std::move(labels.cbegin(), labels.cend(), std::back_inserter(labels_));
+}
+
+void Legend::CreateLegend() {
+
+}
+
+std::vector<sf::RectangleShape> Legend::CreateLabelBoxes() {
+    std::vector<sf::RectangleShape> label_boxes;
+    label_boxes.reserve(labels_.size());
+    for (std::size_t i{0}; i < labels_.size(); i++) {
+        sf::RectangleShape label_box;
+        label_box.setPosition(Config::REFERENCE_POINT);
+//        label_box.
+    }
+    return std::vector<sf::RectangleShape>();
+}
+
+std::vector<sf::Font> Legend::CreateLabelText() {
+    return std::vector<sf::Font>();
+}
+
+sf::RectangleShape Legend::CreateLegendFrame() {
+    sf::RectangleShape legend_frame;
+    legend_frame.setPosition(Config::REFERENCE_POINT);
+    legend_frame.setSize({50, 50});
+    legend_frame.setFillColor(sf::Color::White);
+    legend_frame.setOutlineColor(sf::Color::Black);
+    return legend_frame;
+}
--- Plot/Common/src/legend.h
+++ Plot/Common/src/legend.h
@@ -1,11 +1,25 @@
 #ifndef PLOTINGZ_LEGEND_H
 #define PLOTINGZ_LEGEND_H
 
+#include "Plot/Common/src/config.h"
+
 
 class Legend {
 public:
     /// @todo: For every XY data, there should be a entry in the legend, also, the appropriate colour is used for each data point
-};
+    void SetLegendLabels(const std::vector<std::string> &labels);
 
+    void CreateLegend();
+
+protected:
+    std::vector<sf::RectangleShape> CreateLabelBoxes();
+
+    std::vector<sf::Font> CreateLabelText();
+
+    sf::RectangleShape CreateLegendFrame();
+
+    std::vector<std::string> labels_;
+
+};
 
 #endif //PLOTINGZ_LEGEND_H
