2cf57cf3a09e7d9e8ec87ab1e3e450171fc0b49e
Zvonimir Pervan <z.pervan65@gmail.com>
2020-10-04 14:09:50 +0200

    Line plot: Formated files

--- Plot/LinePlot/src/line_plot.cpp
+++ Plot/LinePlot/src/line_plot.cpp
@@ -8,58 +8,53 @@
 #include <vector>
 
 void LinePlot::CreateLinePlot() {
-    assert(plotting_data_ != nullptr);
+  assert(plotting_data_ != nullptr);
 
-    const auto &input_data = plotting_data_->GetInputDataValuesCollection();
+  const auto &input_data = plotting_data_->GetInputDataValuesCollection();
 
-    std::vector<sf::RectangleShape> lines;
+  std::vector<sf::RectangleShape> lines;
 
-    for (const auto &data : input_data) {
-        for (std::size_t i{0}; i < data.size() - 1; i++) {
-            const auto &normalized_origin_point = NormalizeDataPoint(data.at(i));
-            const auto &origin_point_screen_position = ConvertUtility::ConvertNormalizedPointToAxisScreenSpace(
-                    normalized_origin_point);
+  for (const auto &data : input_data) {
+	for (std::size_t i{0}; i < data.size() - 1; i++) {
+	  const auto &max_x_marker_value = plotting_data_->GetMaxXMarkerValue();
+	  const auto &max_y_marker_value = plotting_data_->GetMaxYMarkerValue();
 
-            const auto &normalized_end_point = NormalizeDataPoint(data.at(i + 1));
-            const auto &end_point_screen_position = ConvertUtility::ConvertNormalizedPointToAxisScreenSpace(
-                    normalized_end_point);
+	  const auto &normalized_origin_point =
+		  ConvertUtility::NormalizeValues(data.at(i), max_x_marker_value, max_y_marker_value);
+	  const auto &origin_point_screen_position = ConvertUtility::ConvertNormalizedPointToAxisScreenSpace(
+		  normalized_origin_point);
 
-            const auto &line_length = MathUtility::CalculateDistanceBetweenTwo2DPoints(
-                    origin_point_screen_position,
-                    end_point_screen_position);
+	  const auto &normalized_end_point =
+		  ConvertUtility::NormalizeValues(data.at(i + 1), max_x_marker_value, max_y_marker_value);
+	  const auto &end_point_screen_position = ConvertUtility::ConvertNormalizedPointToAxisScreenSpace(
+		  normalized_end_point);
 
-            const float angle = MathUtility::CalculateAngleBetweenTwo2DPoints(origin_point_screen_position,
-                                                                              end_point_screen_position);
+	  const auto &line_length = MathUtility::CalculateDistanceBetweenTwo2DPoints(
+		  origin_point_screen_position,
+		  end_point_screen_position);
 
-            sf::RectangleShape line;
-            line.setFillColor(mapped_colors.at(color_count_));
-            line.setSize({line_length, 1});
-            // The rotation is performed around the top-left corner of the screen in CCW direction
-            line.rotate(-angle);
-            line.setPosition(CalculateDataPointPositionOffset(normalized_origin_point));
+	  const float angle = MathUtility::CalculateAngleBetweenTwo2DPoints(origin_point_screen_position,
+																		end_point_screen_position);
 
-            lines.emplace_back(std::move(line));
-        }
-        ++color_count_;
-        plotting_data_->EmplaceDataLinesCollections(lines);
-    }
-}
+	  sf::RectangleShape line;
+	  line.setFillColor(mapped_colors.at(color_count_));
+	  line.setSize({line_length, 1});
+	  // The rotation is performed around the top-left corner of the screen in CCW direction
+	  line.rotate(-angle);
+	  line.setPosition(CalculateDataPointPositionOffset(normalized_origin_point));
 
-sf::Vector2f LinePlot::NormalizeDataPoint(const sf::Vector2f &data_point) const {
-    const auto &max_x_marker_value = plotting_data_->GetMaxXMarkerValue();
-    const float normalized_point_x = ConvertUtility::NormalizeValue(data_point.x, max_x_marker_value);
-
-    const auto &max_y_marker_value = plotting_data_->GetMaxYMarkerValue();
-    const float normalized_point_y = ConvertUtility::NormalizeValue(data_point.y, max_y_marker_value);
-
-    return {normalized_point_x, normalized_point_y};
+	  lines.emplace_back(std::move(line));
+	}
+	++color_count_;
+	plotting_data_->EmplaceDataLinesCollections(lines);
+  }
 }
 
 sf::Vector2f LinePlot::CalculateDataPointPositionOffset(const sf::Vector2f &normalized_point) const {
-    const float offsetted_x_value =
-            Config::REFERENCE_POINT.x + (Config::X_AXIS_DIMENSION.x * normalized_point.x);
-    const float offsetted_y_value =
-            Config::REFERENCE_POINT.y - (Config::Y_AXIS_DIMENSION.y * normalized_point.y);
+  const float offsetted_x_value =
+	  Config::REFERENCE_POINT.x + (Config::X_AXIS_DIMENSION.x * normalized_point.x);
+  const float offsetted_y_value =
+	  Config::REFERENCE_POINT.y - (Config::Y_AXIS_DIMENSION.y * normalized_point.y);
 
-    return {offsetted_x_value, offsetted_y_value};
+  return {offsetted_x_value, offsetted_y_value};
 }
--- Plot/LinePlot/src/line_plot.h
+++ Plot/LinePlot/src/line_plot.h
@@ -5,32 +5,25 @@
 
 /// @brief Contains logic for line plot drawing
 class LinePlot {
-public:
-    explicit LinePlot(PlottingData *plotting_data)
-            : plotting_data_(plotting_data) {}
+ public:
+  explicit LinePlot(PlottingData *plotting_data)
+	  : plotting_data_(plotting_data) {}
 
-    LinePlot() = default;
+  LinePlot() = default;
+  ~LinePlot() = default;
+  LinePlot(const LinePlot &rhs) = delete;
+  LinePlot &operator=(const LinePlot &rhs) = delete;
+  LinePlot(LinePlot &&rhs) noexcept = delete;
+  LinePlot &operator=(LinePlot &&rhs) noexcept = default;
 
-    ~LinePlot() = default;
+  /// @brief Creates line plot data out of given input 2D data points
+  void CreateLinePlot();
 
-    LinePlot(const LinePlot &rhs) = delete;
+ private:
+  sf::Vector2f CalculateDataPointPositionOffset(const sf::Vector2f &normalized_point) const;
 
-    LinePlot &operator=(const LinePlot &rhs) = delete;
-
-    LinePlot(LinePlot &&rhs) noexcept = delete;
-
-    LinePlot &operator=(LinePlot &&rhs) noexcept = default;
-
-    /// @brief Creates line plot data out of given input 2D data points
-    void CreateLinePlot();
-
-private:
-    sf::Vector2f NormalizeDataPoint(const sf::Vector2f &data_point) const;
-
-    sf::Vector2f CalculateDataPointPositionOffset(const sf::Vector2f &normalized_point) const;
-
-    PlottingData *plotting_data_{nullptr};
-    std::size_t color_count_{0};
+  PlottingData *plotting_data_{nullptr};
+  std::size_t color_count_{0};
 };
 
 #endif //PLOTINGZ_LINE_PLOT_H
