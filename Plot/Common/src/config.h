2a123b04712218ebd490a0ad5976bf4e4b0f8d23
Zvonimir Pervan <z.pervan65@gmail.com>
2021-10-30 17:19:32 +0200

    Adjust config parameters

--- Plot/Common/src/config.h
+++ Plot/Common/src/config.h
@@ -9,30 +9,26 @@
 #include "ThirdParty/SFML/include/SFML/Graphics/RectangleShape.hpp"
 #include "ThirdParty/SFML/include/SFML/Graphics/Text.hpp"
 #include "ThirdParty/SFML/include/SFML/System/Vector2.hpp"
-// #include <zconf.h>
 
 using AxisData = std::pair<std::vector<sf::RectangleShape>, std::vector<sf::Text>>;
 
 namespace Config
 {
 
-  static constexpr unsigned int WINDOW_WIDTH{640};
+  static constexpr unsigned int WINDOW_WIDTH{1028};
   static constexpr unsigned int WINDOW_HEIGHT{480};
-  static constexpr unsigned int AXIS_FONT_SIZE{16};
+  static constexpr unsigned int AXIS_FONT_SIZE{14};
 
   static constexpr float MARKER_LENGTH{10.0};
   static constexpr float AXIS_LINE_THICKNESS{2.0};
-  static constexpr float EDGE_OFFSET{80.0};
 
-  static const sf::Vector2f REFERENCE_POINT{WINDOW_WIDTH * 0.1, WINDOW_HEIGHT * 0.9};
-  static const sf::Vector2f X_AXIS_MARKER_REFERENCE_POINT{REFERENCE_POINT.x - MARKER_LENGTH / 2,
-                                                          REFERENCE_POINT.y + AXIS_FONT_SIZE / 2};
-  static const sf::Vector2f Y_AXIS_MARKER_REFERENCE_POINT{REFERENCE_POINT.x - MARKER_LENGTH * 3,
-                                                          REFERENCE_POINT.y - AXIS_FONT_SIZE / 2};
+  static const sf::Vector2f REFERENCE_POINT{WINDOW_WIDTH * 0.05, WINDOW_HEIGHT * 0.9};
+  static const sf::Vector2f X_AXIS_MARKER_REFERENCE_POINT{REFERENCE_POINT.x - MARKER_LENGTH / 2, REFERENCE_POINT.y + AXIS_FONT_SIZE / 2};
+  static const sf::Vector2f Y_AXIS_MARKER_REFERENCE_POINT{REFERENCE_POINT.x - MARKER_LENGTH * 3, REFERENCE_POINT.y - AXIS_FONT_SIZE / 2};
 
   static const sf::Vector2f MARKER_DIMENSION{MARKER_LENGTH, AXIS_LINE_THICKNESS};
-  static const sf::Vector2f X_AXIS_DIMENSION{WINDOW_WIDTH - EDGE_OFFSET, AXIS_LINE_THICKNESS};
-  static const sf::Vector2f Y_AXIS_DIMENSION{AXIS_LINE_THICKNESS, WINDOW_HEIGHT - EDGE_OFFSET};
+  static const sf::Vector2f X_AXIS_DIMENSION{WINDOW_WIDTH - 180.0, AXIS_LINE_THICKNESS};
+  static const sf::Vector2f Y_AXIS_DIMENSION{AXIS_LINE_THICKNESS, WINDOW_HEIGHT - 80.0};
 
   namespace Axis
   {
@@ -43,15 +39,12 @@ namespace Config
   namespace Legend
   {
     static constexpr float FONT_SIZE{12.0};
-    static constexpr float FONT_PIXEL_SCALE{1.33};
+    static constexpr float FONT_PIXEL_SCALE{1.0};
     static constexpr float FRAME_LINE_THICKNESS{1.0};
     static constexpr float LABEL_BOX_OFFSET{1.0};
 
-    static const sf::Vector2f LABEL_BOX_POSITION_OFFSET{5.0, 17.5};
     static const sf::Vector2f LABEL_BOX_SIZE{13.0, 13.0};
-    static const sf::Vector2f LABEL_TEXT_POSITION_OFFSET{LABEL_BOX_POSITION_OFFSET.x * 4,
-                                                         LABEL_BOX_POSITION_OFFSET.y};
-    static const sf::Vector2f FRAME_POSITION_ORIGIN{Config::WINDOW_WIDTH * 0.70, Config::WINDOW_HEIGHT * 0.05};
+    static const sf::Vector2f FRAME_POSITION_ORIGIN{Config::WINDOW_WIDTH * 0.70, Config::WINDOW_HEIGHT * 0.1};
     static const sf::Vector2f FRAME_SIZE{FONT_SIZE, 18};
   } // namespace Legend
 
